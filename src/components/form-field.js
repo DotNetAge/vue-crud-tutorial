bb0bc216313ef6fc276f75ad71bd47cb3484efd1
Ray <csharp2002@hotmail.com>
2016-12-06 23:49:09 +0800

    细化Form-Field示例

--- src/components/form-field.js
+++ src/components/form-field.js
@@ -1,17 +1,56 @@
 export default {
   name: 'FormField',
-  props: [
-    'name',
-    'label'],
+  props: {
+    name: {type: String},
+    label: {type: String},
+    rules: {type: String},
+    value: {},
+    inputType: {type: String, default: 'text'}
+  },
+  mounted () {
+    if (this.rules)
+      validator.attach(this.name, this.rules)
+  },
+  methods: {
+    onValidate (e) {
+      if (this.rules) {
+        this.validator.validate(this.name, e.target.value)
+      }
+    }
+  },
+  computed: {
+    validator () {
+      return this.$parent.$validator
+    },
+    errorBag () {
+      return this.$parent.errors
+    },
+    hasError () {
+      return this.errorBag.has(this.name)
+    },
+    errorMsg() {
+      this.errorBag.first(this.name)
+    }
+  },
   render (h) {
-    return (
-      <div class="uk-form-row">
-        <label class="uk-form-label"
-               for={ this.name }>{ this.label }</label>
-        <div class="uk-form-controls">
-          { this.$slots.default }
-        </div>
+    return <div class="uk-form-row">
+      <label class="uk-form-label"
+             for={this.name}>{this.label}</label>
+      <div class="uk-form-controls">
+        <input id={this.name}
+               name={this.name}
+               type={this.inputType}
+               class={{
+                 'uk-form-width-large': true,
+                 'uk-form-danger': this.hasError
+               }}
+               on-input={ onValidate }
+               value={this.value}/>
+        <small class={{
+          'uk-text-danger': true,
+          'uk-show': this.hasError
+        }}>{ this.errorMsg }</small>
       </div>
-    )
+    </div>
   }
 }
